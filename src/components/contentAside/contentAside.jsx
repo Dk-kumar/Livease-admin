92b54eeaca9b6bc2174552ffe45b1d44c6403e87
Dk-kumar <ayydeepankumar@gmail.com>
2025-10-07 20:53:29 +0530

    route added

--- src/components/contentAside/contentAside.jsx
+++ src/components/contentAside/contentAside.jsx
@@ -1,14 +1,32 @@
 import React, { useState } from "react";
 import "./contentAside.scss";
 import { FiGrid, FiSettings, FiLogOut } from "react-icons/fi";
+import { Link, useLocation } from "react-router-dom";
 
 const ContentAside = () => {
+  const { pathname } = useLocation();
+
   // Menu data (can later come from API or config file)
   const [menuItems] = useState([
-    { id: 1, label: "Dashboard",icon: "/assets/Vector.png", active: true },
-    { id: 2, label: "User Management", icon: "/assets/Vector.png" },
-    { id: 3, label: "Payments & Wallet", icon: "/assets/Vector.png" },
-    { id: 4, label: "Reports", icon: "/assets/Vector.png" },
+    { id: 1, label: "Dashboard", icon: "/assets/Vector.png", link: "/" },
+    {
+      id: 2,
+      label: "User Management",
+      icon: "/assets/Vector.png",
+      link: "/user-management",
+    },
+    {
+      id: 3,
+      label: "Payments & Wallet",
+      icon: "/assets/Vector.png",
+      link: "/wallet",
+    },
+    {
+      id: 4,
+      label: "Reports",
+      icon: "/assets/Vector.png",
+      link: "support-ticket",
+    },
     { id: 5, label: "Analytics", icon: "/assets/Vector.png" },
   ]);
 
@@ -25,32 +43,31 @@ const ContentAside = () => {
       {/* Menu */}
       <ul className="menu">
         {menuItems.map((item) => (
-          <li key={item.id} className={item.active ? "active" : ""}>
-            <img src={item.icon} alt={item.label} className="menu-icon" />
-            <span>{item.label}</span>
-          </li>
+          <Link to={item?.link}>
+            <li
+              key={item.id}
+              className={item?.link === pathname ? "active" : ""}
+            >
+              <img src={item.icon} alt={item.label} className="menu-icon" />
+              <span>{item.label}</span>
+            </li>
+          </Link>
         ))}
       </ul>
 
       {/* Footer actions */}
       <div className="footer">
         <div className="footer-item">
-          <img
-            src="/assets/Vector (1).png"
-            alt="avatar"
-          />
+          <img src="/assets/Vector (1).png" alt="avatar" />
           <span>Settings</span>
         </div>
         <div className="footer-item logout">
-          <img
-            src="/assets/3d_avatar_12.png"
-            alt="avatar"
-          />
+          <img src="/assets/3d_avatar_12.png" alt="avatar" />
           <span>Log out</span>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default ContentAside;
--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Header from '../header/header';
-import Aside from '../contentAside/contentAside';
-import './homepage.scss';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../header/header";
+import Aside from "../contentAside/contentAside";
+import "./homepage.scss";
 
 function HomePage() {
   return (
-    <div className='container'>
-      <Header />
+    <div className="container">
+      <div>
+        <Header />
+      </div>
       <Aside />
       <main>
         <Outlet /> {/* This will render the routed content */}
